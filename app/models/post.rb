e68ce45c2461e4ae58d67f0c6217f1ecf75c0ccb
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-04-25 20:14:40 -0500

    commit tree 38c25472c26e4cead8db5ec2ab8e434ecd38472a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :title_is_clickbaity?
+
+    CLICKBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+
+    def title_is_clickbaity?
+        if CLICKBAIT.none? {|phrase| phrase.match title}
+            errors.add(:title, "needs more clickbait!!!")
+        end
+    end
 end
